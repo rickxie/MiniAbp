76001d0e1bcaefe50fbcabe86b7ccff847bc8955
rickxiemc <rickxie@users.noreply.github.com>
2016-09-06 14:36:51 +0800

    MapTo支持泛型和 枚举类型。

--- src/MiniAbp.Console/Program.cs
+++ src/MiniAbp.Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Specialized;
 using Castle.Components.DictionaryAdapter;
 using Castle.DynamicProxy;
+using MiniAbp.Extension;
 using Newtonsoft.Json;
 
 namespace MiniAbp.Console
@@ -10,19 +11,44 @@ namespace MiniAbp.Console
     {
         private static void Main(string[] args)
         {
+            var listNV = new Genric<NameVal>();
+            var nameVal = new NameVal();
+            nameVal.Name = "xiaoming";
+            nameVal.Value = "happy";
+            listNV.Model = nameVal;
+            var listNV2 = new Genric<NameVal>();
+            var nameVal2 = new NameVal();
+            nameVal2.Name = "333";
+            nameVal2.Value = "333";
+            listNV2.Model = nameVal;
+
+            var newN = listNV.MapTo(listNV2);
+            newN.Model.Name = "444";
+        }
+
+        private void AopTest()
+        {
             Dictionary<string, List<NameVal>> nv = new Dictionary<string, List<NameVal>>();
-            nv.Add("admin05", new EditableList<NameVal>() {new NameVal() {Name = "Name", Value = "xiaoming"},new NameVal() {Name = "Sex", Value = "0"} });
-            nv.Add("admin01", new EditableList<NameVal>() {new NameVal() {Name = "Name", Value = "XX"},new NameVal() {Name = "Sex", Value = "1"} });
+            nv.Add("admin05", new EditableList<NameVal>() { new NameVal() { Name = "Name", Value = "xiaoming" }, new NameVal() { Name = "Sex", Value = "0" } });
+            nv.Add("admin01", new EditableList<NameVal>() { new NameVal() { Name = "Name", Value = "XX" }, new NameVal() { Name = "Sex", Value = "1" } });
             var jsong = JsonConvert.SerializeObject(nv);
             System.Console.Read();
-
         }
     }
 
+    public class Genric<T>
+    {
+        public T Model { get; set; }
+    }
     public class NameVal
     {
         public string Name { get; set; }
         public string Value { get; set; }
+    }
+
+    public class NameA
+    {
+        public string Name { get; set; }
     }
     public interface ICar
     {
--- src/MiniAbp/Extension/ObjectExtension.cs
+++ src/MiniAbp/Extension/ObjectExtension.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Castle.Core.Internal;
 using Castle.MicroKernel.Registration;
+using MiniAbp.DataAccess;
 using Newtonsoft.Json;
 
 namespace MiniAbp.Extension
@@ -13,27 +16,89 @@ namespace MiniAbp.Extension
         public static TDestination MapTo<TDestination>(this object source) where TDestination : class, new()
         {
             var targetInstance = new TDestination();
-            return source.MapTo(targetInstance);
+            if (IsEnumerableType(source.GetType()))
+            {
+                MapToGenericEnumerable(source, targetInstance);
+            }
+            else
+            {
+                MapToSimpleObject(source, targetInstance);
+            }
+            return targetInstance;
         }
 
         /// <summary>
         /// Execute a mapping from the source object to the existing destination object
-        ///             There must be a mapping between objects before calling this method.
+        /// There must be a mapping between objects before calling this method.
         /// 
         /// </summary>
         /// <typeparam name="TSource">Source type</typeparam><typeparam name="TDestination">Destination type</typeparam><param name="source">Source object</param><param name="destination">Destination object</param>
         /// <returns/>
         public static TDestination MapTo<TSource, TDestination>(this TSource source, TDestination destination) where TDestination : class, new()
         {
+            if (IsEnumerableType(typeof(TSource)))
+            {
+                throw new ArgumentException("This method can't support class which inherit IEnumberable`1");
+            }
+            if(destination == null) destination = new TDestination();
+            MapToSimpleObject(source, destination);
+            return destination;
+        }
 
-//            var serializedSource = JsonConvert.SerializeObject(source);
-//            var deserializedSource = JsonConvert.DeserializeObject<TDestination>(serializedSource);
+        /// <summary>
+        /// Does class implement IEnumerable
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static bool IsEnumerableType(Type type)
+        {
+            var hasInterface = type.GetInterface("IEnumerable`1");
+            return hasInterface != null;
+        }
+
+        /// <summary>
+        /// Map to generic type which inherit IEnumerable
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        private static void MapToGenericEnumerable<TSource, TDestination>(this TSource source, TDestination destination)
+            where TDestination : class, new()
+        {
+            var sourceType = source.GetType();
+            var destType = destination.GetType();
+            var sourceHasInterface = sourceType.GetInterface("IEnumerable`1");
+            var targetHasInterface = destType.GetInterface("IEnumerable`1");
+            if (sourceHasInterface != null && targetHasInterface != null && sourceType.IsGenericType)
+            {
+                int count = Convert.ToInt32(sourceType.GetProperty("Count").GetValue(source, null));
+                for (int i = 0; i < count; i++)
+                {
+                    var itemValue = sourceType.GetProperty("Item").GetValue(source, new object[] {i});
+                    var destGenericType = destType.GenericTypeArguments[0];
+                    var destInstance = destGenericType.Assembly.CreateInstance(destGenericType.FullName);
+                    MapToSimpleObject(itemValue, destInstance);
+                    var methodAdd = destType.GetMethod("Add");
+                    methodAdd.Invoke(destination, new object[] {destInstance});
+                }
+            }
+        }
+        /// <summary>
+        /// Map to simple object
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        private static void MapToSimpleObject<TSource, TDestination>(this TSource source, TDestination destination)
+            where TDestination : class, new()
+        {
             var sourceType = source.GetType();
             var sourceProps = sourceType.GetProperties();
-            var targetType = typeof(TDestination);
+            var targetType = destination.GetType();
             var targetProps = targetType.GetProperties();
-            var targetInstance = destination;
-
             foreach (var p in sourceProps)
             {
                 var targetValueType = targetProps.FirstOrDefault(r => r.Name == p.Name);
@@ -41,13 +106,20 @@ namespace MiniAbp.Extension
                 {
                     var valueType = sourceType.GetProperty(p.Name);
                     var value = valueType.GetValue(source);
-                    if (value != null)
+                    if(value == null)
+                        continue;
+                    var preSetValue = value;
+                    //only support simple object type not support collection
+                    if (!valueType.PropertyType.IsValueType && valueType.PropertyType != typeof (String))
                     {
-                        targetValueType.SetValue(targetInstance, value);
+                        var targetPropType = targetValueType.PropertyType;
+                        var targetPropInstance = targetPropType.Assembly.CreateInstance(targetPropType.FullName);
+                        MapToSimpleObject(value, targetPropInstance);
+                        preSetValue = targetPropInstance;
                     }
+                    targetValueType.SetValue(destination, preSetValue);
                 }
             }
-            return targetInstance;
         }
     }
 }
